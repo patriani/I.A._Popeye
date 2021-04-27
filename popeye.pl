d7201af41e3c7318645f4734d28c1f49e8a13325
Ricardo Bortolucci Patriani de Carvalho <43487367+patriani@users.noreply.github.com>
2021-04-26 23:37:02 -0300

    RecursãoInfinita
    quando a pergunta: "?-formaListaObjetivos(ListaObjetivos)" é feita o limite de dados para resposta de aprox. 1GB é atingido.

--- popeye.pl
+++ popeye.pl
@@ -1,68 +1,92 @@
 
 
+
 % ---------------------------------------------- Regras Mov {s()} -----
 
 
 %% mov direita
 
-s([X,Y], [Xprox,Y],ListaEscadas):- X<9, Xprox is X + 1. % adicionar not(pertence([Xprox,Y],ListaGarrafa)).
+s([X,Y], [Xprox,Y]):- X<9, Xprox is X + 1. % adicionar not(pertence([Xprox,Y],ListaGarrafa)).
 %implementar sucessão para salto
 
 %% mov esquerda
-s([X,Y], [Xprox,Y],ListaEscadas):- X>0, Xprox is X - 1. % adicionar not(pertence([Xprox,Y],ListaGarrafa)). --> anda uma casa se estiver livre
+s([X,Y], [Xprox,Y]):- X>0, Xprox is X - 1. % adicionar not(pertence([Xprox,Y],ListaGarrafa)). --> anda uma casa se estiver livre
 % implementar sucessão para salto --> proxima casa está ocupada por
 % garrafa
 
 
 
 
-% ---------------------------------------------------------------------
+% -------------------------------------------Fatos mapa:
 
+escada([2,1]).
+escada([3,2]).
+escada([5,2]).
+escada([4,3]).
+escada([9,3]).
+escada([10,4]).
+escada([7,4]).
+escada([6,5]).
+
+coracao([8,2]).
+coracao([10,3]).
+coracao([8,4]).
+coracao([10,5]).
+
+posicaoEspinafre([9,5]).
+posicaoBrutus([10,5]).
+
+%------------------------------------Formatação dos fatos:
+
+
+formaListaCoracoes(ListaCoracoes):-bagof(Coordenada,coracao(Coordenada),ListaCoracoes).
+
+%A condição de parada desse bagof() será: acabaram os fatos escada(Coordenada).
+formaListaEscada(ListaEscadas):-bagof(Coordenada,escada(Coordenada),ListaEscadas).
+
+formaListaObjetivos(ListaObjetivos):-concatena(LE,PE,R1),concatena(R1,PB,ListaObjetivos),formaListaEscada(LE),posicaoEspinafre(PE),posicaoBrutus(PB).
+
+%-----------------------------------Algoritmos Dependencias:
+
+concatena([Cabeca|Cauda],L2,[Cabeca|Resultado]):-concatena(Cauda,L2,Resultado).
+concatena([],L,L).
 
 meta(X,Y):- X = Y. % [XAtual,YAtual] = [XObjetivo,YObjetivo]
 
-estende([EstadoAtual|Caminho],ListaSucessores,ListaEscadas):-
-	bagof([Sucessor,EstadoAtual|Caminho], (s(EstadoAtual,Sucessor,ListaEscadas), not(pertence(Sucessor,[EstadoAtual|Caminho]))), ListaSucessores),!. %pesquisar
+estende([EstadoAtual|Caminho],ListaSucessores):-
+	bagof([Sucessor,EstadoAtual|Caminho], (s(EstadoAtual,Sucessor), not(pertence(Sucessor,[EstadoAtual|Caminho]))), ListaSucessores),!. %pesquisar
 
 estende(_,[],_). %revisar
 
 pertence(E, [E|_]).
 pertence(E, [_|Cauda]):- pertence(E, Cauda).
 
-% codigo bl:
+% --------------------------------------------codigo bl():
 
-solucao_bl(EstadoInicial, ListaEscadas, Objetivo, CaminhoSolucao):-bl([[EstadoInicial]], ListaEscadas, Objetivo, CaminhoSolucao). %encapsula
+solucao_bl(EstadoInicial, CaminhoSolucao):-bl([[EstadoInicial]], CaminhoSolucao). %encapsula
 
-bl([[EstadoAtual|Caminho]|_],ListaEscadas,Objetivo,[EstadoAtual|Caminho]):-
-    meta(EstadoAtual,Objetivo). %achou resultado na cabeça da lista de análise: forma "CaminhoSolução" com EstadoAtual + Caminho percorrido até ele.
+bl([[EstadoAtual|Caminho]|_],[EstadoAtual|Caminho]):-
+    meta(EstadoAtual). %achou resultado na cabeça da lista de análise: forma "CaminhoSolução" com EstadoAtual + Caminho percorrido até ele.
 
-bl([Cabeca|Cauda],ListaEscadas,Objetivo, CaminhoSolucao) :-
-	estende(Cabeca,Sucessores,ListaEscadas),
+bl([Cabeca|Cauda], CaminhoSolucao) :-
+	estende(Cabeca,Sucessores),
 	concatena(Cauda,Sucessores,NovaFronteira),
-	bl(NovaFronteira,ListaEscadas,Objetivo, CaminhoSolucao).
+	bl(NovaFronteira, CaminhoSolucao).
 
-% [[X,Y],...]
+%--------------------------------------------- main():
+main(EstadoInicial, CaminhoSolucao) :-
+	solucao_bl(EstadoInicial, CaminhoSolucao).
 
-main(EstadoInicial, ListaEscadas, PosicaoEspinafre, CaminhoSolucao) :-
-	solucao_bl(EstadoInicial, ListaEscadas, PosicaoEspinafre, CaminhoSolucao).
 
-	% ?- main([1,1],[[[2,1],[2,2]]],[9,3],S).
 
-%!
-%escada([2,1]).
-%escada([3,2]).
-%escada([5,2]).
-%escada([4,3]).
-%escada([9,3]).
-%escada([10,4]).
-%escada([7,4]).
-%escada([6,5]).
-%
-%posicaoEspinafre([9,5]).
-%posicaoBrutus([10,5]).
-%
-% %A condição de parada desse bagof() será: acabaram os fatos escada(Coordenada).
-% formaListaEscada(ListaEscadas):-bagof(Coordenada,escada(Coordenada),ListaEscadas).
-%
-% formaListaObjetivos():-concatena(ListaEscadas,PosicaoEspinafre,R1),concatena(R1,PosicaoBrutus,R2).
-%
+
+
+
+
+
+
+
+
+
+
+
