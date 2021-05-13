aaf81ab5d3f3034276f2d5579a9c3be5e8a7a169
Ricardo Bortolucci Patriani de Carvalho <43487367+patriani@users.noreply.github.com>
2021-05-13 01:50:17 -0300

    Update popeye.pl
    Cumpre com os requisitos de funcionamento.

--- popeye.pl
+++ popeye.pl
@@ -1,43 +1,42 @@
 % ---------------------------------------------- Regras Mov {s()} -----
 
 %% ---------------------mov direita:
-%
-s([X,Y], [Xprox,Y],_,_,ListaGarrafa):-
-    X<10,
-    Xprox is X + 1,
-    not(pertence([Xprox,Y],ListaGarrafa)),
-    posicaoBrutus(PB),
-    not(pertence([Xprox,Y],PB)).
+%implementação utilizando a chamada padrão de regra de sucessão:
 
-s([X,Y], [Xprox,Y],[CabecaObjetivos|_],_,_):-
+s([X,Y], [Xprox,Y],_,_,ListaGarrafa):- % caso de movimentação mais simples para direita
+    X<10, % as condições de tamanho do X e do Y limitam o movimento dando ilusão de mapa em execução
+    Xprox is X + 1, % incremento do X simula movimento horizontal (eixo X) para direita
+    not(pertence([Xprox,Y],ListaGarrafa)), % essa regra não aceita quando a próxima casa à direita da atual é uma garrafa
+    posicaoBrutus(PB),
+    not(pertence([Xprox,Y],PB)). % essa regra não é utilizada para "derrotar" o Brutus (passar por ele)
+
+s([X,Y], [Xprox,Y],[CabecaObjetivos|_],_,_):- % movimentação para direita quando o Brutus está na próxima casa
     X<10,
     Xprox is X + 1,
     posicaoBrutus(PB),
     [CabecaObjetivos] == PB,
-    pertence([Xprox,Y],PB).
+    pertence([Xprox,Y],PB).% essa condição faz com que essa seja a única regra de movimentação para direita que trate o caso do Brutus ser a casa seguinte
 
-
-
-s([X,Y], [Xprox2,Y],_,_,ListaGarrafa):-
-    X<9,
+s([X,Y], [Xprox2,Y],_,_,ListaGarrafa):- % salto para direita
+    X<9, % o limite máximo da casa atual é 8 para essa regra porque a movimentação ocorre em incremento de 2
     Xprox is X + 1,
     pertence([Xprox,Y],ListaGarrafa),
     Xprox2 is X + 2,
-    not(pertence([Xprox2,Y],ListaGarrafa)),
+    not(pertence([Xprox2,Y],ListaGarrafa)), % essas ultimas linhas especificam que não há salto quando existem duas garrafas consecutivas ou garrafa+Brutus
     posicaoBrutus(PB),
     not(pertence([Xprox2,Y],PB)).
 
 %% ---------------------mov esquerda:
 %
-s([X,Y], [Xprox,Y],[CabecaObjetivos|_],_,ListaGarrafa):-
-    X>1,
+s([X,Y], [Xprox,Y],[CabecaObjetivos|_],_,ListaGarrafa):- % movimentação para esquerda quando o Brutus está na próxima casa
+    X>1, % em movimentações para esquerda o X deve ser maior que um porque um decremento levaria à posição 0 que eh considerada como fora do limite do mapa
     Xprox is X - 1,
     not(pertence([Xprox,Y],ListaGarrafa)),
     posicaoBrutus(PB),
     [CabecaObjetivos] == PB,
     pertence([Xprox,Y],PB).
 
-s([X,Y], [Xprox,Y],_,_,ListaGarrafa):-
+s([X,Y], [Xprox,Y],_,_,ListaGarrafa):- % caso mais simples de movimentação para esquerda
     X>1,
     Xprox is X - 1,
     not(pertence([Xprox,Y],ListaGarrafa)),
@@ -45,7 +44,7 @@ s([X,Y], [Xprox,Y],_,_,ListaGarrafa):-
     not(pertence([Xprox,Y],PB)).
 
 
-s([X,Y], [Xprox2,Y],_,_,ListaGarrafa):-
+s([X,Y], [Xprox2,Y],_,_,ListaGarrafa):- % salto para esquerda
     X>2,
     Xprox is X -1,
     pertence([Xprox,Y],ListaGarrafa),
@@ -56,7 +55,7 @@ s([X,Y], [Xprox2,Y],_,_,ListaGarrafa):-
 
 %%---------------------mov cima direita:
 %
-s([X,Y], [Xprox,Yprox],_,ListaEscadas,_):-
+s([X,Y], [Xprox,Yprox],_,ListaEscadas,_):- % movimentação em escada subindo para diagonal direita
     X<10,
     Y<5,
     pertence([X,Y],ListaEscadas),
@@ -66,7 +65,7 @@ s([X,Y], [Xprox,Yprox],_,ListaEscadas,_):-
 
 %%---------------------mov cima esquerda:
 %
-s([X,Y], [Xprox,Yprox],_,ListaEscadas,_):-
+s([X,Y], [Xprox,Yprox],_,ListaEscadas,_):- % movimentação em escada subindo para diagonal esquerda
     X>1,
     Y<5,
     pertence([X,Y],ListaEscadas),
@@ -76,7 +75,7 @@ s([X,Y], [Xprox,Yprox],_,ListaEscadas,_):-
 
 %%--------------------mov baixo direita:
 %
-s([X,Y], [Xprox,Yprox],_,ListaEscadas,_):-
+s([X,Y], [Xprox,Yprox],_,ListaEscadas,_):- % movimentação em escada descendo para direita
     X<10,
     Y>1,
     Xprox is X + 1,
@@ -86,7 +85,7 @@ s([X,Y], [Xprox,Yprox],_,ListaEscadas,_):-
 
 %%---------------------mov baixo esquerda:
 %
-s([X,Y], [Xprox,Yprox],_, ListaEscadas,_):-
+s([X,Y], [Xprox,Yprox],_, ListaEscadas,_):- % movimentação em escada descendo para esquerda
     X>1,
     Y>1,
     Xprox is X - 1,
@@ -94,60 +93,46 @@ s([X,Y], [Xprox,Yprox],_, ListaEscadas,_):-
     pertence([X,Y],ListaEscadas),
     pertence([Xprox,Yprox],ListaEscadas).
 
-
-
-
-
-% adicionar not(pertence([Xprox,Y],ListaGarrafa)). --> anda uma casa se estiver livre
-% implementar sucessão para salto --> proxima casa está ocupada por
-% garrafa
-
-% -------------------------------------------Tamanho da lista:
-
-tamanho([],0).
-tamanho([_|Cauda],N):-
-        tamanho(Cauda,N1),
-        N is N1+1.
-
 % -------------------------------------------Fatos mapa:
-%escada([2,1]).
-%escada([3,2]).
-%escada([5,2]).
-%escada([4,3]).
-%escada([9,3]).
-%escada([10,4]).
-%escada([7,4]).
-%escada([6,5]).
 
-escada([1,2]).
-escada([2,1]).
-escada([5,2]).
-escada([4,1]).
+escada([3,1]).
+escada([4,2]).
+escada([2,2]).
+escada([3,3]).
+escada([7,3]).
+escada([6,4]).
+escada([3,4]).
+escada([4,5]).
 
-coracao([4,1]).
-%coracao([4,2]).
-%coracao([10,3]).
-%coracao([8,4]).
-%coracao([10,5]).
+coracao([10,2]).
+coracao([5,3]).
+coracao([1,4]).
+coracao([3,5]).
 
-garrafa([]).
+garrafa([5,1]).
+garrafa([6,1]).
+garrafa([2,3]).
+garrafa([9,4]).
+garrafa([2,5]).
+garrafa([6,5]).
 
+posicaoEspinafre([[8,3]]). % posição dentro de uma lista para unificar com a lista formada por corações na construção de Lista de Objetivos
+posicaoBrutus([[10,5]]). % posição dentro de uma lista para unificar com a lista formada por corações na construção de Lista de Objetivos
 
-posicaoEspinafre([]). %posições iguais para teste
-posicaoBrutus([[3,1]]).
-
-%  TODO: implementar regra para não permitir dois objetos na mesma
-%  posição.
-
-%------------------------------------Formatação dos fatos:
-
-
-formaListaCoracoes(ListaCoracoes):-bagof(Coordenada,coracao(Coordenada),ListaCoracoes).
+%------------------------------------Organização dos fatos:
 
 %A condição de parada desse bagof() será: acabaram os fatos escada(Coordenada).
+formaListaCoracoes(ListaCoracoes):-bagof(Coordenada,coracao(Coordenada),ListaCoracoes).
+
 formaListaEscadas(ListaEscadas):-bagof(Coordenada,escada(Coordenada),ListaEscadas).
 
 formaListaGarrafas(ListaGarrafas):-bagof(Coordenada,garrafa(Coordenada),ListaGarrafas).
+
+% "formaListaObjetivos()" implementa a lista de objetivos colocando em
+% ordem:
+%    1. Coletar Coracoes
+%    2. Comer Espinafre
+%    3. Derrotar Brutus
 
 formaListaObjetivos(R2):-
         formaListaCoracoes(LC),
@@ -156,10 +141,16 @@ formaListaObjetivos(R2):-
         posicaoBrutus(PB),
         concatena(R1,PB,R2).
 
+%---------------------------------------Regra de Pontuação:
+
+pontuacao([X,Y],Pontuacao,PontuacaoAtualizada):-
+    formaListaCoracoes(Coracoes),
+    pertence([X,Y],Coracoes), % consulta se o objetivo atual passado pelo meta é um coracao
+    PontuacaoAtualizada is (Pontuacao + Y*100). % um coração vale 100 vezes o valor do andar que se encontra
 
 %-----------------------------------Algoritmos Dependencias:
 
-concatena([Cabeca|Cauda],L2,[Cabeca|Resultado]):-concatena(Cauda,L2,Resultado).
+concatena([Cabeca|Cauda],L2,[Cabeca|Resultado]):-concatena(Cauda,L2,Resultado). % regra padrão de concatenação de elementos
 concatena([],L,L).
 
 % ! estende() chama bagof() de forma que o sucessor eh escolhido a
@@ -178,41 +169,49 @@ pertence(E, [_|Cauda]):- pertence(E, Cauda).
 
 % --------------------------------------------codigo bl():
 
-solucao_bl(EstadoInicial, CaminhoSolucao,ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas):-bl([[EstadoInicial]], CaminhoSolucao,ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas). %encapsula
+solucao_bl(EstadoInicial, CaminhoSolucao,ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas,PontuacaoCorrente,PontuacaoFinal):-bl([[EstadoInicial]], CaminhoSolucao,ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas,PontuacaoCorrente,PontuacaoFinal). % encapsula EstadoInicial para unificações durante a busca
 
 
-% - A solução unifica com [EstadoAtual|Caminho] pq se o meta eh true a
-% solução eh o estado atual.
-bl([[EstadoAtual|Caminho]|_],CaminhoSolucao,ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas):-
-        meta([EstadoAtual|Caminho],CaminhoSolucao,ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas). %achou resultado na cabeça da lista de análise: forma "CaminhoSolução" com EstadoAtual + Caminho percorrido até ele.
+% Este passo da Busca em Largura verifica se algum objetivo foi
+% atingido, inclusive verifica se a busca terminou (Lista de Objetivos
+% vazia)
+bl([[EstadoAtual|Caminho]|_],CaminhoSolucao,ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas,PontuacaoCorrente,PontuacaoFinal):-
+        meta([EstadoAtual|Caminho],CaminhoSolucao,ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas,PontuacaoCorrente,PontuacaoFinal). %achou resultado na cabeça da lista de análise: forma "CaminhoSolução" com EstadoAtual + Caminho percorrido até ele.
 
-bl([Cabeca|Cauda], CaminhoSolucao, ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas):-
+% Caso a busca não tenha sido concluida a busca em largura expande o
+% caminho alternativo na cabeça da lista de caminhos em busca de um
+% objetivo.
+bl([Cabeca|Cauda], CaminhoSolucao, ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas,PontuacaoCorrente,PontuacaoFinal):-
 	estende(Cabeca,ListaSucessores,ListaObjetivos,ListaEscadas,ListaGarrafas),
 	concatena(Cauda,ListaSucessores,NovaFronteira),
-	bl(NovaFronteira, CaminhoSolucao, ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas).
+	bl(NovaFronteira, CaminhoSolucao, ListaObjetivos,CaminhoTemp,ListaEscadas,ListaGarrafas,PontuacaoCorrente,PontuacaoFinal).
 
-meta([EstadoAtual|Caminho],CaminhoSolucao,[CabecaObjetivo|CaudaObjetivo],CaminhoTemp,_,_):-
+meta([EstadoAtual|Caminho],CaminhoSolucao,[CabecaObjetivo|CaudaObjetivo],CaminhoTemp,_,_,PontuacaoCorrente,PontuacaoFinal):-
         EstadoAtual == CabecaObjetivo, % o estado atual realmente corresponde a um objetivo
-        CaudaObjetivo == [], % É o ultimo objetivo (deverá ser o Brutus) e a busca pode terminar
-        concatena([EstadoAtual|Caminho],CaminhoTemp,CaminhoSolucao).
+        CaudaObjetivo == [], % É o ultimo objetivo e a busca pode terminar
+        concatena([EstadoAtual|Caminho],CaminhoTemp,CaminhoSolucao),
+        PontuacaoFinal is PontuacaoCorrente. % impede que a pontuação seja zerada no retorno da recursão
 
 %separa a lista de objetivo em CabecaObjetivo e CaudaObjetivo
-meta([EstadoAtual|Caminho],CaminhoSolucao,[CabecaObjetivo|CaudaObjetivo],CaminhoTemp,ListaEscadas,ListaGarrafas):-
+meta([EstadoAtual|Caminho],CaminhoSolucao,[CabecaObjetivo|CaudaObjetivo],CaminhoTemp,ListaEscadas,ListaGarrafas,PontuacaoCorrente,PontuacaoFinal):-
         EstadoAtual == CabecaObjetivo, % o estado atual realmente corresponde a um objetivo
         not(CaudaObjetivo == []),
-        concatena([EstadoAtual|Caminho],CaminhoTemp,CaminhoTemp2),
-        solucao_bl(EstadoAtual,CaminhoSolucao,CaudaObjetivo,CaminhoTemp2,ListaEscadas,ListaGarrafas),!. %sem esse corte a busca continua para outro meta e acaba falhando a busca% toda
+        pontuacao(CabecaObjetivo,PontuacaoCorrente,PontuacaoAtualizada),
+        concatena([EstadoAtual|Caminho],CaminhoTemp,CaminhoTemp2), % guarda o caminho correto até o objetivo atual permitindo que o popeye repita coordenadas durante sua movimentação até o próximo objetivo.
+        solucao_bl(EstadoAtual,CaminhoSolucao,CaudaObjetivo,CaminhoTemp2,ListaEscadas,ListaGarrafas,PontuacaoAtualizada,PontuacaoFinal),!. %sem esse corte a busca continua para outro meta e acaba falhando a busca toda
 
+meta([EstadoAtual|Caminho],CaminhoSolucao,[CabecaObjetivo|CaudaObjetivo],CaminhoTemp,ListaEscadas,ListaGarrafas,PontuacaoCorrente,PontuacaoFinal):-
+        EstadoAtual == CabecaObjetivo, % o estado atual realmente corresponde a um objetivo
+        not(CaudaObjetivo == []),
+        formaListaCoracoes(ListaCoracoes),
+        not(pertence(EstadoAtual,ListaCoracoes)),
+        concatena([EstadoAtual|Caminho],CaminhoTemp,CaminhoTemp2),
+        solucao_bl(EstadoAtual,CaminhoSolucao,CaudaObjetivo,CaminhoTemp2,ListaEscadas,ListaGarrafas,PontuacaoCorrente,PontuacaoFinal),!.
 
 %--------------------------------------------- main():
 %
-main(EstadoInicial,CaminhoSolucao) :-
+main(EstadoInicial,CaminhoSolucao,PontuacaoFinal) :-
         formaListaObjetivos(ListaObjetivos),
         formaListaEscadas(ListaEscadas),
         formaListaGarrafas(ListaGarrafas),
-	solucao_bl(EstadoInicial, CaminhoSolucao, ListaObjetivos,[],ListaEscadas,ListaGarrafas).
-
-
-
-
-
+	solucao_bl(EstadoInicial, CaminhoSolucao, ListaObjetivos,[],ListaEscadas,ListaGarrafas,0,PontuacaoFinal).
